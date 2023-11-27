58bb19a0f85fac296ea6f5801215d93f1939a8b8
meT1moffey <96593529+meT1moffey@users.noreply.github.com>
2023-11-27 11:46:56 +0300

    Adaptive rules rect

--- Assets/Scripts/RuleGenerator.cs
+++ Assets/Scripts/RuleGenerator.cs
@@ -10,6 +10,7 @@ public class RuleGenerator : MonoBehaviour
 
     void Start()
     {
+        GetComponent<RectTransform>().sizeDelta = prefab.GetComponent<RectTransform>().sizeDelta * new Vector2(1, rules.Length) * 1.2f;
         for (int i = 0; i < rules.Length; i++)
         {
             RuleButton rule = Instantiate(prefab, transform);
